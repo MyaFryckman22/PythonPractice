f57087351c6d11cbb62ec997f095fb0f52079a56
Fryckman Mya <817258@stma.k12.mn.us>
2021-09-13 15:09:12 -0500

    yo

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+print("AHHHHHHHHHHHHH")
+print("god has let me live another day and I'm about to make it EVERYONES problem")
+
+#print first movie 
+print("Knives out")
+
+#print 2nd movie 
+print("Rashamon")
+
+#print 3rd favorite movie 
+print("King of Comedy")
