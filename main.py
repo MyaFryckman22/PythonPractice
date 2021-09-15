39937c136666f877f24e5474d53c2f53fc4520fa
MyaFryckman22 <817258@mystma.org>
2021-09-15 14:41:07 -0500

    I added more top five lists

--- main.py
+++ main.py
@@ -8,4 +8,59 @@ def topFiveMovies():
 
     #print third movie
     print("King of Comedy")   
-topFiveMovies()
+topFiveMovies() 
+
+#define function 
+def topfivefryproviders():
+    #print first fry
+    print("Space Aliens")
+
+    #print second fry
+    print("Dairy Queen")
+
+    #print third fry
+    print("Willy Mccoy's")
+
+    #print fourth fry
+    print("Culvers")
+
+    #print fifth fry
+    print("Disco Fries R.I.P.")
+topfivefryproviders()
+
+#define function 
+def topfivetechpositions():
+    #print first position
+    print("Stage Manager")
+
+    #print second position 
+    print("Director")
+
+    #print third position 
+    print("Tech Superviser")
+
+    #print fourth position
+    print("ASM")
+
+    #print fifth position
+    print("Deck Chief")
+topfivetechpositions()
+
+#define function 
+def topfiveKBNGmoment():
+    #print first 
+    print("The Worm")
+
+    #print second fry
+    print("Noah and Tommy's departure")
+
+    #print third fry
+    print("Bulg")
+
+    #print fourth fry
+    print("Doing the news")
+
+    #print fifth fry
+    print("...")
+topfiveKBNGmoment()
+
